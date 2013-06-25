67be6f5206ab286203626ba22f4b233a2a16c478
caiguanhao <caiguanhao@gmail.com>
2013-06-25 23:28:18 +0800

    add message seed;

---
+++ db/seeds_messages.rb
@@ -0,0 +1,3 @@
+10.times { |i|
+  Message.create([{ name: "Sheldon Cooper", gender: 0, phone_number: "123456", qq_number: "123456", title: "What is Ruby?", content: "Ruby is a dynamic, reflective, general-purpose object-oriented programming language that combines syntax inspired by Perl with Smalltalk-like features. It was also influenced by Eiffel and Lisp. Ruby was first designed and developed in the mid-1990s by Yukihiro \"Matz\" Matsumoto in Japan."}])
+}
