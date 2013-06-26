45c2a5a31770d8b491b3e2f426a6cb6ef6f4f2b3
caiguanhao <caiguanhao@gmail.com>
2013-06-27 03:41:27 +0800

    can now set locale; add some Chinese translations;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  LOCALES = { en: "English", zh_CN: "中文" }
+
   def not_found
     render :file => "#{::Rails.root}/public/404", :layout => false, :status => :not_found
   end
@@ -11,6 +13,17 @@ class ApplicationController < ActionController::Base
     define_method "is_#{role}?" do
       return false unless current_user.present?
       current_user.role == role
+    end
+  end
+
+  before_filter :set_locale
+ 
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      cookies['locale'] = { :value => params[:locale], :expires => 1.year.from_now }
+      I18n.locale = params[:locale].to_sym
+    elsif cookies['locale'] && I18n.available_locales.include?(cookies['locale'].to_sym)
+      I18n.locale = cookies['locale'].to_sym
     end
   end
 
